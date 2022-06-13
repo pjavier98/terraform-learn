fbc7cfe6b80ea4351ea44a9bd8c09977c15573ef
Pedro Javier Paneca Cordova <pjpc@outlook.com.br>
2022-06-13 14:38:11 -0400

    use aws default security group

--- main.tf
+++ main.tf
@@ -57,8 +57,40 @@ resource "aws_default_route_table" "learn-app-route-table" {
   }
 }
 
-resource "aws_security_group" "learn-app-security-group" {
-  name   = "learn-app-security-group"
+# resource "aws_security_group" "learn-app-security-group" {
+#   name   = "learn-app-security-group"
+#   vpc_id = aws_vpc.learn-app-vpc.id
+
+#   ingress {
+#     description = "TLS from VPC"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = [var.my_ip_address]
+#   }
+
+#   ingress {
+#     description = "Browser from VPC"
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     cidr_blocks     = ["0.0.0.0/0"]
+#     prefix_list_ids = []
+#   }
+
+#   tags = {
+#     Name = "${var.environment}-learn-app-security-group"
+#   }
+# }
+
+resource "aws_default_security_group" "learn-app-security-group" {
   vpc_id = aws_vpc.learn-app-vpc.id
 
   ingress {
