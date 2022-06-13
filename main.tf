3c9d557d5acb2b5760e052e63d44e47e8746236f
Pedro Javier Paneca Cordova <pjpc@outlook.com.br>
2022-06-13 13:55:37 -0400

    use aws default route table

--- main.tf
+++ main.tf
@@ -26,8 +26,26 @@ resource "aws_internet_gateway" "learn-app-internet-gateway" {
   }
 }
 
-resource "aws_route_table" "learn-app-route-table" {
-  vpc_id = aws_vpc.learn-app-vpc.id
+# resource "aws_route_table" "learn-app-route-table" {
+#   vpc_id = aws_vpc.learn-app-vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.learn-app-internet-gateway.id
+#   }
+
+#   tags = {
+#     Name = "${var.environment}-learn-app-route-table"
+#   }
+# }
+
+# resource "aws_route_table_association" "association-route-table-subnet" {
+#   subnet_id      = aws_subnet.learn-app-subnet.id
+#   route_table_id = aws_route_table.learn-app-route-table.id
+# }
+
+resource "aws_default_route_table" "learn-app-route-table" {
+  default_route_table_id = aws_vpc.learn-app-vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -37,9 +55,4 @@ resource "aws_route_table" "learn-app-route-table" {
   tags = {
     Name = "${var.environment}-learn-app-route-table"
   }
-}
-
-resource "aws_route_table_association" "association-route-table-subnet" {
-  subnet_id      = aws_subnet.learn-app-subnet.id
-  route_table_id = aws_route_table.learn-app-route-table.id
 }
