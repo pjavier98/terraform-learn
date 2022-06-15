7faa2db0d1daac2fc7e4cf38a40029a6834b7efa
Pedro Javier Paneca Cordova <pjpc@outlook.com.br>
2022-06-14 22:36:04 -0400

    configure ec2 server to run entry script sh and docker container

--- main.tf
+++ main.tf
@@ -138,6 +138,8 @@ resource "aws_instance" "learn-app-server" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
 
+  user_data = file("entry-script.sh")
+
   tags = {
     Name = "${var.environment}-learn-app-server"
   }
