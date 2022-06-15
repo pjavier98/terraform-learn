bc75b0b4818ff9547fe9a6695cbe4ec66652f098
Pedro Javier Paneca Cordova <pjpc@outlook.com.br>
2022-06-15 14:34:42 -0400

    configure remote storage with aws s3 for state.tfstate

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "terraform-learn-app"
+    key    = "terraform-learn-app/state.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
